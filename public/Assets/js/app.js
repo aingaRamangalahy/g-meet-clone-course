634ca5766ddd6902f1e59b2dfc1feb1046bfcbde
ainga Ramangalahy <aingaramangalahy@gmail.com>
2022-03-18 08:22:23 +0300

    app js init

---
+++ public/Assets/js/app.js
@@ -0,0 +1,12 @@
+var MyApp = (function() {
+
+    function init(userId, meetingId) {
+
+    }
+
+    return {
+        _init: function(userId, meetingId) {
+            init(userId, meetingId)
+        }
+    }
+})();
